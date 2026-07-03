89cc45c29dd671b0f961b9fe74295f252ee83663
Zaryab Ayaz <zaryabayaz2@gmail.com>
2026-07-03 16:23:26 +1000

    done imu

--- week_8/src/IMUGyroscope.hpp
+++ week_8/src/IMUGyroscope.hpp
@@ -9,16 +9,35 @@ class IMUGyroscope {
 public:
     IMUGyroscope() : mpu6050{Wire} {}
 
+    /*
+    Initialises the MPU, blocks indefinitely if unable to connect to
+    the MPU. Call this method at the beginning of main.cpp:setup.
+    */
     void begin(void) {
         Wire.begin();
         if (mpu6050.begin() != 0) {
             Serial.println("Could not connect to MPU6050");
+            while (true);
         }
+        mpu6050.calcOffsets();
+    }
+
+    /*
+    Resets the IMU's gyroscope calibration, use whenever changing targets.
+    */
+    void reset(void) {
+        mpu6050.setGyroOffsets(0.0f, 0.0f, 0.0f);
+    }
+
+    /*
+    Returns heading of the IMU, yet to determine units etc.
+    */
+    float getHeading() {
+        return mpu6050.getAngleZ();
     }
 
 private:
     MPU6050 mpu6050;
-
 };
 
 }
